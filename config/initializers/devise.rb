bb640a81001e7222110cd961c7461831bf821896
ramyalus <ramya20lu@gmail.com>
2024-09-03 19:49:43 +0530

    Google credentials configuration

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -311,7 +311,7 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
 
-  config.omniauth :google_oauth2, Rails.application.credentials.dig(:google, :client_id), Rails.application.credentials.dig(:google, :client_secret),   
+  config.omniauth :google_oauth2, '509414509249-0p5l71ge921kth31leijg5m4q9f0h8tt.apps.googleusercontent.com', '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',   
   {
     scope: 'userinfo.email, userinfo.profile',
     prompt: 'select_account',
